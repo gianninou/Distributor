a781eb3cdc3c58833f2a3bed955774cebc15be10
Jean-Philippe Eisenbarth <max@nurupoga.org>
2015-05-10 22:32:34 +0200

    structure RemoteClient complétée

--- src/remoteClient.c
+++ src/remoteClient.c
@@ -1,18 +1,30 @@
 #include "remoteClient.h"
 
-RemoteClient* newRemoteClient(int id){
-	RemoteClient* rm = (RemoteClient*)xmalloc(sizeof(RemoteClient));
-	rm->id=id;
-	return rm;
+RemoteClient* newRemoteClient(struct sockaddr_in dialog_socket){
+	static int current_id = 0;
+	RemoteClient* rc = NULL;
+
+	char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+	size_t size = sizeof dialog_socket;
+	socklen_t len = (socklen_t)size;
+
+	if (getnameinfo((struct sockaddr*) &dialog_socket, len, hbuf, sizeof(hbuf), sbuf,
+	                sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV) == 0) {
+		rc = (RemoteClient*)xmalloc(sizeof(RemoteClient));
+		rc->id = current_id++;
+		rc->dialog_socket = dialog_socket;
+		strcpy(rc->ip, hbuf);
+		strcpy(rc->port, sbuf);
+		strcpy(rc->data_to_send, "NULL");
+		rc->timestamp_last_pong_sent = -1;
+	}
+	return rc;
 }
 
-
-void deleteRemoteClient(RemoteClient* rm){
-	free(rm);
+void deleteRemoteClient(RemoteClient* rc){
+	free(rc);
 }
 
-
-void printRemoteClient(RemoteClient* rm){
-	printf("RM : id=%d\n",rm->id );
+void printRemoteClient(RemoteClient* rc){
+	printf("RemoteM : id = %d\n ip = %s \n port = %s \n data to send = %s \n last pong sent = %d\n", rc->id, rc->ip, rc->port, rc->data_to_send, rc->timestamp_last_pong_sent);
 }
-
--- src/remoteClient.h
+++ src/remoteClient.h
@@ -3,14 +3,25 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
 
 #include "utils.h"
 
+#define BUFFERSIZE 256
+
 typedef struct remoteClient{
 	int id;
+	struct sockaddr_in dialog_socket;
+	char ip[NI_MAXHOST];
+	char port[NI_MAXSERV];
+	char data_to_send[BUFFERSIZE];
+	int timestamp_last_pong_sent;
+
 }RemoteClient;
 
-RemoteClient* newRemoteClient(int id);
+RemoteClient* newRemoteClient(struct sockaddr_in dialog_socket);
 void deleteRemoteClient(RemoteClient* rm);
 void printRemoteClient(RemoteClient* rm);
 
