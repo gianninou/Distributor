ddc499a40db805a3314105ae71e7bbbcca629de4
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2015-05-14 10:45:24 +0200

    modification pour eviter les gros calculs

--- algo/friedman.py
+++ algo/friedman.py
@@ -2,15 +2,24 @@
 # coding: utf-8
 
 import sys
-
+import time
 from itertools import*
+from threading import Thread
+
+
 
 def friedman(nb) :
     res = ''
     s=lambda x:[x]['1'>x>'0':]+['(%s%s%s)'%f for i in range(1,len(x))for f in product(s(x[:i]),'*/-+^',s(x[i:]))]
     def E(e):
         try:
-            return eval(e.replace("^","**"))
+
+            string = "a = "+e.replace("^","**")
+            if(len(e.split('^',))<=2):
+                code = compile(string, '<string>', 'exec')
+                ns = {}
+                exec code in ns
+                return ns['a']
         except:
             0
     A={i:e for i in range(nb,nb+1)for x in permutations(`i`)for e in s("".join(x))[x>='1':]if E(e)==i}
