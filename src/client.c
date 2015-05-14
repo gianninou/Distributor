cbb724a6ff5a7252e56cb6d36ef18012e66098da
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2015-05-14 10:45:56 +0200

    reindent

--- src/client.c
+++ src/client.c
@@ -5,72 +5,72 @@ static int thread_continu;
 
 void *thread_ping(void *arg){
     /* Pour enlever le warning */
-    (void) arg;
+	(void) arg;
 
-    int socketServeur;
+	int socketServeur;
 
-    socketServeur=socket(AF_INET,SOCK_DGRAM,0);
-    if(socketServeur<0){
-        perror("error socket");
-        exit(1);
-    }
+	socketServeur=socket(AF_INET,SOCK_DGRAM,0);
+	if(socketServeur<0){
+		perror("error socket");
+		exit(1);
+	}
 
 
-    struct sockaddr_in addr_r;
-    memset(&addr_r,0,sizeof(addr_r));
-    addr_r.sin_family=AF_INET;
-    addr_r.sin_port=htons(PORT_MULTICAST);
-    addr_r.sin_addr.s_addr=htonl(INADDR_ANY);
+	struct sockaddr_in addr_r;
+	memset(&addr_r,0,sizeof(addr_r));
+	addr_r.sin_family=AF_INET;
+	addr_r.sin_port=htons(PORT_MULTICAST);
+	addr_r.sin_addr.s_addr=htonl(INADDR_ANY);
 
-    struct ip_mreq imr;
-    imr.imr_multiaddr.s_addr=inet_addr(GROUP_MULTICAST);
-    imr.imr_interface.s_addr=INADDR_ANY;
+	struct ip_mreq imr;
+	imr.imr_multiaddr.s_addr=inet_addr(GROUP_MULTICAST);
+	imr.imr_interface.s_addr=INADDR_ANY;
 
-    if(setsockopt(socketServeur,IPPROTO_IP,IP_ADD_MEMBERSHIP,&imr,sizeof(imr))<0){
-        perror("settruc err");
-        exit(1);
-    }
+	if(setsockopt(socketServeur,IPPROTO_IP,IP_ADD_MEMBERSHIP,&imr,sizeof(imr))<0){
+		perror("settruc err");
+		exit(1);
+	}
 
-    unsigned long on=1;
-    if(setsockopt(socketServeur,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(imr))<0){
-        perror("settruc err reuse");
-        exit(1);
-    }
+	unsigned long on=1;
+	if(setsockopt(socketServeur,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(imr))<0){
+		perror("settruc err reuse");
+		exit(1);
+	}
 
-    if(bind(socketServeur,(struct sockaddr*)&addr_r,sizeof(addr_r))<0){
-        perror("err bind");
-        exit(1);
-    }
+	if(bind(socketServeur,(struct sockaddr*)&addr_r,sizeof(addr_r))<0){
+		perror("err bind");
+		exit(1);
+	}
 
-    int cnt;
-    socklen_t len_r;
-    char buff[100];
+	int cnt;
+	socklen_t len_r;
+	char buff[100];
 
 
     /* Boucle d'attente des PING */
-    while(thread_continu){
-    	len_r=sizeof(addr_r);
-	    cnt=recvfrom(socketServeur,buff,sizeof(buff),0,(struct sockaddr*)&addr_r,&len_r);
-	    if(cnt<0){
-	        perror("error");
-	        exit(1);
-	    }
+	while(thread_continu){
+		len_r=sizeof(addr_r);
+		cnt=recvfrom(socketServeur,buff,sizeof(buff),0,(struct sockaddr*)&addr_r,&len_r);
+		if(cnt<0){
+			perror("error");
+			exit(1);
+		}
 
-	    if(!strncmp(buff,"PIN",3)){
-	    	printf("PING recu\n");
-	    	sprintf(buff,"PON");
-	    	cnt=sendto(socketServeur,buff,strlen(buff),0,(struct sockaddr*)&addr_r,len_r);
-	    	if(cnt!=strlen(buff)){
-	    		printf("erreur thread sendto\n");
-	    	}
-	    }else{
-	    	printf("ERREUR thread recu : %s\n",buff );
-	    }
+		if(!strncmp(buff,"PIN",3)){
+			printf("PING recu\n");
+			sprintf(buff,"PON");
+			cnt=sendto(socketServeur,buff,strlen(buff),0,(struct sockaddr*)&addr_r,len_r);
+			if(cnt!=strlen(buff)){
+				printf("erreur thread sendto\n");
+			}
+		}else{
+			printf("ERREUR thread recu : %s\n",buff );
+		}
 
 	}
 
 
-    pthread_exit(NULL);
+	pthread_exit(NULL);
 } 
 
 
@@ -110,13 +110,13 @@ int main(int argc, char* argv[]){
 			break;
 		switch (c){
 			case 'n':
-				numb = atoi(optarg);
-				break;
+			numb = atoi(optarg);
+			break;
 			case 'v':
-				verbose_flag++;
-				break;
+			verbose_flag++;
+			break;
 			default:
-				abort ();
+			abort ();
 		}
 	}
 
@@ -159,9 +159,9 @@ int main(int argc, char* argv[]){
 	/* Lancement du thread */
 	/***********************/
 	if(pthread_create(&thread, NULL, thread_ping, NULL) == -1) {
-	    perror("pthread_create");
-	    return EXIT_FAILURE;
-    } 
+		perror("pthread_create");
+		return EXIT_FAILURE;
+	} 
 
 
 	/* s'enregistrer sur le serveur, récuperer son ID */
